dfde3a1e4aed585dbf149893cd79e07d569d1437
swethasuresh-08 <swethamadan0821@gmail.com>
2024-06-13 21:35:55 +0530

    Book My Show FrontEnd

---
+++ frontend_bms/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Book My Show Front end</p>
+    </div>
+  );
+}
+
+export default App;
