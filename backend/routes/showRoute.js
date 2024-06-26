c0582677df69e8ff10b42ae97319ddb25ea265f1
swethasuresh-08 <swethamadan0821@gmail.com>
2024-06-26 12:29:38 +0530

    Show feature Update BE

---
+++ backend/routes/showRoute.js
@@ -0,0 +1,108 @@
+const router=require("express").Router()
+const Show=require("../models/showModel")
+const authMiddleware=require('../middleware/authMiddleware')
+
+router.get("/get-all-shows-by-theatre-id/:theatreId", authMiddleware, async (req, res) => {
+    try {
+        const shows = await Show.find({theatre:req.params.theatreId}).populate("movie").populate("theatre")
+        console.log(req.params.theatreId)
+        
+        res.status(200).send({
+            success: true,
+            message: "shows fetched",
+            shows
+        })
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "There was some issue in fetching shows for user.",
+        })
+    }
+})
+
+// router.get("/get-all-theatres", async (req, res) => {
+//     try {
+//         const theatres = await Theatre.find().populate("owner","-password")
+
+//         res.status(200).send({
+//             success: true,
+//             message: "Theatres fetched",
+//             theatres
+//         })
+//     } catch (error) {
+//         res.status(500).send({
+//             success: false,
+//             message: "There was some issue in fetching theatres for user.",
+//         })
+//     }
+// })
+
+router.post("/add-show",async (req,res)=>{
+    try{
+        const newShow=new Show(req.body)
+        await newShow.save()
+
+        res.status(200).json({success:true,message:"New show has been created"})
+
+    }
+    catch(error)
+    {
+        res.status(500).send({
+            success:false,
+            message:"There was some error in adding shows"
+        })
+    }
+})
+
+// router.get("/get-theatre",authMiddleware,async (req,res)=>{
+//     try{
+//         const theatres=await Theatre.find()
+//         res.status(200).json({
+//             success:true,
+//             message:"Theatres Fetched",
+//             theatres
+//         })
+//     }catch(error)
+//     {
+//         res.status(500).json({
+//             success:false,
+//             message:"There was error fetching movies"
+//         })
+//     }
+// })
+
+// router.post("/update-theatre",authMiddleware,async (req,res)=>{
+//     try{
+//        await Theatre.findByIdAndUpdate(req.body.theatreId,req.body)
+//         res.send({
+//             success:true,
+//             message:"Theatre has been found by id and updated"
+//         })
+
+//     }
+//     catch(error)
+//     {
+//         res.send({
+//             success:false,
+//             error:"Something went wrong"
+//         })
+//     }
+// })
+
+router.post("/delete-show",authMiddleware,async (req,res)=>{
+    try{
+        await Show.findByIdAndDelete(req.body.showId)
+        res.send({
+            success:true,
+            message:"Show Deleted successfully"
+        })
+    }
+    catch(error)
+    {
+        res.send({
+            success:false,
+            message:"Something went wrong"
+        })
+    }
+})
+module.exports=router
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const cors=require('cors')
 const userRoute=require('../backend/routes/userRoute')
 const movieRoute=require('./routes/movieRoute')
 const theatreRoute=require('./routes/theatreRoute')
+const showRoute=require('./routes/showRoute')
+
 const PORT=5000
 
 //Always add the config file and den import db config
@@ -17,6 +19,7 @@ app.use(cors())
 app.use("/api/users",userRoute)
 app.use("/api/movies",movieRoute)
 app.use("/api/theatres",theatreRoute)
+app.use("/api/shows",showRoute)
 //Start Server
 app.listen(PORT,()=>{
     console.log("Server started successfully")
